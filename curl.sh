053a76ec21212d706a3ee041186c20c55d8a3a9b
fabianvarisco <fabianvarisco@gmail.com>
2020-03-15 01:34:34 -0300

    getBalance rpc example (wip)

--- curl.sh
+++ curl.sh
@@ -46,7 +46,9 @@ exe eth_pendingTransactions
 
 exe eth_pendingTransactions '["pending"]'
 
-exe eth_getTransactionCount '["0xfe3b557e8fb62b89f4916b721be55ceb828dbd73", "pending"]'
+exe eth_getBalance '["0x198Dc5869055b73037A118ab576884813E1b6e93", "latest"]'
+
+exe eth_getTransactionCount '["0x198Dc5869055b73037A118ab576884813E1b6e93", "pending"]'
 
 exe eth_getTransactionByHash '["0x9f565e05eac245a9b70cf728f02575c42526246311101dcf35f4df9b3d62628d"]'
 
